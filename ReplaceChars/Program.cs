03c08d513e71cb69e46b416f3a999dc4b833b83c
Andrew Byerly <andrew.Byerly@idcomlog.com>
2018-01-03 13:38:01 -0600

    Added SafeEncodeString method

--- ReplaceChars/Program.cs
+++ ReplaceChars/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics.Contracts;
+using System.Globalization;
 
 namespace ReplaceChars
 {
@@ -14,8 +15,34 @@ namespace ReplaceChars
             while (true) { 
                 Console.Write("Enter Text: ");
                 string inputText = Console.ReadLine();
-
+                Console.WriteLine("Output: " + SafeEncodeString(inputText));
             }
+        }
+
+        public static string SafeEncodeString(string encodeString)
+        {
+            if (encodeString == null)
+                return null;
+
+            encodeString = StripDiacritics(encodeString);
+
+            // this misses some, so here's a few other replacements.
+            encodeString = encodeString.Replace("\u00E6", "ae");
+            encodeString = encodeString.Replace("\u00C6", "AE");
+            encodeString = encodeString.Replace("\u00DF", "ss");
+            encodeString = encodeString.Replace("ß", "ss");
+            encodeString = encodeString.Replace("’", "'");
+            encodeString = encodeString.Replace("é", "e");
+
+            // replace a few other Unicode characters as needed
+            encodeString = encodeString.Replace("\u00A9", "(C)");
+            encodeString = encodeString.Replace("\u00AE", "(R)");
+            encodeString = encodeString.Replace("\u2122", "(tm)");
+
+            // lastly, remove any and all unicode characters that are still lingering about.
+            string.Concat(encodeString.Normalize(NormalizationForm.FormD).Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark));
+
+            return encodeString;
         }
 
         [Pure]
